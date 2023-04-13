1a4bdf9e53f60d3a6ad29d9b1bbe651550c48e57
OdinTaccom <neil@abdigital.ca>
2023-04-13 06:20:08 -0400

    April 13 morning updates

---
+++ amplayerapp/amplayerapp/Pages/register_athlete.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace amplayerapp.Pages
+{
+    public class register_athlete : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
